92418664edddda4cdb2c170f2781a2884fb99006
youssefhani11 <joo01305@gmail.com>
2024-11-09 18:41:13 +0200

    first commit

---
+++ CarpetCleaningService/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Carpet_Cleaning_Service
+{
+    internal class Program
+    {
+        private static int largeCarpets;
+
+        static void Main(string[] args)
+        {
+            int smallCarpets = int.Parse (Console.ReadLine());
+            Console.WriteLine($"small carpets is :{smallCarpets}");
+            int largeCarpets  = int .Parse (Console.ReadLine());
+            Console.WriteLine($"large carpets is :{largeCarpets}");
+            int priceSmall  = 25;
+            int priceLarge = 35;
+
+            Console.WriteLine($"price small room : $ {priceSmall}");
+            Console.WriteLine($"price large room : $ {priceLarge}");
+
+            int costsmall;
+            int costlarge;
+            costsmall = smallCarpets * priceSmall;
+            Console.WriteLine($"cost small is {costsmall}");
+            costlarge = largeCarpets * priceLarge;
+            Console.WriteLine($"cost large is {costlarge}");
+            int TotalCost=costsmall+costlarge;
+            Console.WriteLine(TotalCost);
+
+            Console.WriteLine(" tax : $6.6 ");
+            Console.WriteLine("Total estimate: $116.6\r\nThis estimate is valid for 30 days");
+
+            Console.WriteLine("----------------------------------------------------------------");
+        }
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+            int Y = 20;
+            Console.WriteLine($"Equation: {X} + {Y} = {X + Y:C}");
+
+            // احنا هنا عرفنا متغير x or y 
+            // وعملنا عليهم عمليه جمع 
+            // واستخدمنا :c 
+            // ف اضافه تنسيق العمله 
+
+        }
+    }
+}
