d71219eb5799e647c4783062622d9ef6cc0f7c65
youssefhani11 <joo01305@gmail.com>
2024-11-09 18:56:09 +0200

    add readme file and gitgnore file

--- CarpetCleaningService/obj/Debug/net8.0/Carpet Cleaning Service.AssemblyInfo.cs
+++ CarpetCleaningService/obj/Debug/net8.0/Carpet Cleaning Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Carpet Cleaning Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92418664edddda4cdb2c170f2781a2884fb99006")]
 [assembly: System.Reflection.AssemblyProductAttribute("Carpet Cleaning Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Carpet Cleaning Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
